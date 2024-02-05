2fa929fbab85dd61de9b9126225f8cc035cd2155
mosesmkrs <karisamoses392@gmail.com>
2024-02-05 18:28:14 +0300

    /

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 
 const getTopics = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/topics", {
+    const res = await fetch("http://localhost:3000/api/topics/", {
       cache: "no-store",
     });
 
